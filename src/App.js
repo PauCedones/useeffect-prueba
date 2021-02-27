7ea2ed5f00026a2afb7708786228a1092ae13838
Paula <cedones@gmail.com>
2021-02-26 21:13:14 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { useEffect, useState } from "react";
+import CatImg from "./components/CatImg";
 
 function App() {
-  const [image, setImage] = useState("");
-
-  //traer data de una API
-  async function fetchData() {
-    const getData = await fetch("https://api.thecatapi.com/v1/images/search");
-    const getJson = await getData.json();
-    //llaman al useState aqui
-    console.log(getJson);
-    setImage(getJson[0].url);
-  }
+  const [gatitoImg, setGatitoImg] = useState("");
 
   useEffect(() => {
-    fetchData();
+    fetchGatitos();
   }, []);
 
-  return (
-    <>
-      <img src={image} alt="gatito random" width="400" />
-    </>
-  );
+  async function fetchGatitos() {
+    const data = await fetch("https://api.thecatapi.com/v1/images/search");
+    const dataJson = await data.json();
+
+    setGatitoImg(dataJson[0].url);
+  }
+
+  return <CatImg url={gatitoImg} />;
 }
 
 export default App;
